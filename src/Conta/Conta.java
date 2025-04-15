d39bb8f054a4d9302a75f7e1496b96eed3d8aeed
Rodolfo-Pessoa <rodolfolujcaspessoa@hotmail.com>
2025-04-15 19:02:44 -0300

    Primeiro commit

--- src/Conta/Conta.java
+++ src/Conta/Conta.java
@@ -1,4 +1,115 @@
 package Conta;
 
+import java.util.Scanner;
+
 public class Conta {
+
+    private int contaBanco;
+    private String nomeCliente;
+    private String resposta;
+    private double totalConta;
+    private double deposito;
+    private double saque;
+
+
+    Scanner sc = new Scanner(System.in);
+
+    public Conta() {
+        this.contaBanco = contaBanco;
+        this.nomeCliente = nomeCliente;
+    }
+
+    public int getContaBanco() {
+        return contaBanco;
+    }
+
+    public void setContaBanco(int contaBanco) {
+        this.contaBanco = contaBanco;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getResposta() {
+        return resposta;
+    }
+
+    public void setResposta(String resposta) {
+
+        if (resposta.equals("y")) {
+            System.out.println("Informe o valor do deposito: ");
+            double valor;
+            valor = sc.nextDouble();
+
+            setTotalConta(valor);
+
+            System.out.println("Dados da conta: ");
+            System.out.println("Conta: "
+                    + getContaBanco()
+                    + ",  Priprietário: " + getNomeCliente()
+                    + ", Saldo: " + getTotalConta());
+            System.out.println("Gostaria de fazer mais alguma opração de saque ou deposito? (y/n)");
+            resposta = sc.nextLine();
+            sc.nextLine();
+
+                if (resposta == "y") {
+                    System.out.println("Informe o valor: ");
+                    sc.nextLine();
+
+                }else {
+                    System.out.println("Informe o valor: ");
+                    sc.nextLine();
+                }
+        } else {
+            System.out.println("Dados da conta: ");
+            System.out.println("Conta: "
+                    + getContaBanco()
+                    + ",  Priprietário: " + getNomeCliente()
+                    + ", Saldo: " + getTotalConta());
+            System.out.println("Gostaria de fazer alguma opração de saque ou deposito? (y/n)");
+            resposta = sc.nextLine();
+            sc.nextLine();
+                if (resposta.equals("y")) {
+                    System.out.println("1 - Saque");
+                    System.out.println("2 - Deposito");
+                    sc.nextLine();
+                }
+        }
+    }
+    public double getTotalConta() {
+        return totalConta;
+    }
+
+    public void setTotalConta(double totalConta) {
+        this.totalConta = totalConta;
+    }
+
+    public double getDeposito() {
+        return deposito;
+    }
+
+    public void setDeposito(double deposito) {
+        this.deposito = deposito;
+    }
+
+    public double getSaque() {
+        return saque;
+    }
+
+    public void setSaque(double saque) {
+        this.saque = saque;
+    }
+    public void depositar() {
+
+    }
+    public void sacar() {
+        if (totalConta <= 0.00 || saque <= totalConta) {
+            System.out.println("Valor insuficiente");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.util.Locale;
+import Conta.Conta;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Conta c1 = new Conta();
+
+        System.out.print("Informe o Nº da conta:");
+            c1.setContaBanco(sc.nextInt());
+        sc.nextLine();
+
+        System.out.print("Informe o nome do cliente: ");
+            c1.setNomeCliente(sc.nextLine());
+
+        System.out.println("É o primeiro deposito? (y/n)");
+            c1.setResposta(sc.nextLine());
     }
 }
